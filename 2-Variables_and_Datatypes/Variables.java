c6ddfafe51e2ad2393f9d1a2e2ffc19baa6d95d2
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-04-22 19:17:55 +0530

    Add Day-2 files

---
+++ 2-Variables_and_Datatypes/Variables.java
@@ -0,0 +1,15 @@
+
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 20;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Sushmitha Shettigar";
+        System.out.println(name);
+        a = 80;
+        System.out.println(a);
+        a=b;
+        System.out.println(a);
+    }
+}
