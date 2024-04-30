bbcedc587c8642ede088d216ff8723a106e6d912
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-04-30 18:36:15 +0530

    Add Day-10 files

---
+++ 8-Advanced_Patterns/InvertedHalfPyramid_Num.java
@@ -0,0 +1,13 @@
+public class InvertedHalfPyramid_Num{
+    public static void invertedHalfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        invertedHalfPyramid(5);
+    }
+}
