bf3e9ffd1ad96861dbc61e45de7bd4c3ec595700
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-02 07:34:57 +0530

    Add Day-11 files

---
+++ 8-Advanced_Patterns/NumberPyramid.java
@@ -0,0 +1,18 @@
+public class NumberPyramid{
+    public static void number_pyramid(int n){
+        for(int i=1;i<=n;i++){
+            //space
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        number_pyramid(5);
+    }
+}
