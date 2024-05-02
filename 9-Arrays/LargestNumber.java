c5191562776a957ab974c1106d43182e73308422
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-02 16:28:09 +0530

    Add Day-12 files

---
+++ 9-Arrays/LargestNumber.java
@@ -0,0 +1,24 @@
+//Find the largest number in the given array.
+
+public class LargestNumber {
+    public static int getLargest(int numbers[]){
+        int largest = Integer.MIN_VALUE;// -infinity
+        int smallest = Integer.MAX_VALUE; // +infinity
+
+        for(int i=0;i<numbers.length;i++){
+            if(largest < numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("Smallest value is : "+ smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1,2,6,3,5};
+
+        System.out.println("Largest value is : " + getLargest(numbers));
+    }
+}
