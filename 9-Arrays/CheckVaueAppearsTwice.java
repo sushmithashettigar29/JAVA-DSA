4bd0c3412cf4e753dcfca19c6d49bfee5ca4c642
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-07 07:22:55 +0530

    Add Day-17 files

--- 9-Arrays/CheckVaueAppearsTwice.java
+++ 9-Arrays/CheckVaueAppearsTwice.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+
 public class CheckVaueAppearsTwice {
     public static boolean checkCondition(int nums[]){
         //answer is not correct - not optimized
-        for(int i=0;i<nums.length;i++){
-            for(int j= i+1 ;j<nums.length;j++){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
+        Arrays.sort(nums);
+        for(int i=0;i < nums.length-1;i++){
+            // for(int j=i+1 ; j < nums.length;j++){
+            //     if(nums[i] == nums[j]){
+            //         return true;
+            //     }
+            // }
+            if(nums[i] == nums[i+1]){
+                return true;
             }
-           
         }
         return false;
     }
