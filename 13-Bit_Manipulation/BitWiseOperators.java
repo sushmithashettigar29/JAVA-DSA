2cb6cc3b7e6160bfdd0a8af5ea228b30a65129f2
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-14 19:04:35 +0530

    Add Day-24 files

---
+++ 13-Bit_Manipulation/BitWiseOperators.java
@@ -0,0 +1,10 @@
+public class BitWiseOperators{
+    public static void main(String[] args) {
+        System.out.println("5 & 6 : "+ (5&6));
+        System.out.println("5 | 6 : "+ (5|6));
+        System.out.println("5 ^ 6 : "+ (5^6));
+        System.out.println("~5 : "+ (~5));//-6
+        System.out.println("5 << 2: "+ (5<<2));
+        System.out.println("6 >> 1: "+ (6>>1));
+    }
+}
