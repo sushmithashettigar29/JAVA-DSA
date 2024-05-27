a80ad67c011b67c0d172acab5c02de2e6821d8fc
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-05-27 07:16:08 +0530

    Add Day-37 files

---
+++ 15-Recursion/FindLastOccurance.java
@@ -0,0 +1,17 @@
+public class FindLastOccurance {
+    public static int lastOccurance(int arr[],int key,int i){
+         if(i==arr.length-1){
+            return -1;
+         }
+         int isFound = lastOccurance(arr, key, i+1);
+         if(isFound == -1 && arr[i] == key){
+            return i;
+         }
+         return isFound;
+    }
+    public static void main(String[] args) {
+        int arr[] = {8,3,6,9,5,10,2,5,3};
+        int key = 5;
+        System.out.println(lastOccurance(arr, key, 0));
+    }
+}
---
+++ 15-Recursion/PrintXPowerN.java
@@ -0,0 +1,25 @@
+public class PrintXPowerN {
+    public static int power(int x,int n){//O(n)
+        if(n == 0){
+            return 1;
+        }
+        return x * power(x, n-1);
+    }
+    public static int optimizedPower(int x, int n){//O(log n)
+        if(n==0){
+            return 1;
+        }
+        int halfPower = optimizedPower(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        // n is odd
+        if(n % 2 != 0){
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
+    }
+    public static void main(String[] args) {
+    System.out.println(power(2, 10));
+    System.out.println(optimizedPower(2, 10));
+    }
+}
