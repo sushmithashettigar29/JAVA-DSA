340ee8bc0cedc5c3f69d19baf8238413c9de9392
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-06-25 19:26:23 +0530

    Add Day-53 files

---
+++ 19-ArrayList/WaterContainer_TwoPointer.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class WaterContainer_TwoPointer {
+
+    public static int storeWater(ArrayList<Integer> height){
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+        //2 pointer approach = O(n)
+        while(lp < rp){
+            //calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currWater = ht * width;
+            maxWater = Math.max(maxWater, currWater);
+
+            //update pointer
+            if(height.get(lp) < height.get(rp)){
+                lp++;
+            }else{
+                rp--;
+            }
+        }
+
+        return maxWater;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        //1,8,6,2,5,4,8,3,7
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(storeWater(height));
+    }
+}
