bd8d475d69c70c0e4c97a9477970d8c1870fdb2c
sushmithashettigar29 <sushmithashettigar5@gmail.com>
2024-06-26 19:27:18 +0530

    Add Day-54 files

---
+++ 20-LinkedList/LinkedList.java
@@ -0,0 +1,64 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //step1 = create new node
+        Node newNode = new Node(data);
+        //If linkedlist is empty
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        //step2 = newNode next = Head
+        newNode.next = head; //linking
+        //step3  = head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print(){
+        if(head == null){
+            System.out.println("LinkedList is empty.");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data+ "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        // ll.head = new Node(1);
+        // ll.head.next = new Node(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+    }
+}
